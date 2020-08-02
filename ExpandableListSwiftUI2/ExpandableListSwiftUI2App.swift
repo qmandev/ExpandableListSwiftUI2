18497d0d186c334e438e2c7b4778f0f4596538bc
Qiang Ma <qiangma7@gmail.com>
2020-08-01 20:45:19 -0400

    Initial Commit

---
+++ ExpandableListSwiftUI2/ExpandableListSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableListSwiftUI2App.swift
+//  ExpandableListSwiftUI2
+//
+//  Created by Qiang Ma on 8/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableListSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
